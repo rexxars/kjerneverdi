45dc682e337cc4333ba06ddfc5755bf1306bbeb7
Roy Hvaara <roy@lightyear.no>
2021-03-29 14:38:07 -0700

    Use getStaticProps and revalidate hourly for core values component (#3)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,27 @@
+import {GetStaticProps, GetStaticPropsResult} from 'next'
 import {getCoreValue} from '../helpers/values'
 
-export default function Home() {
-  return (
-    <div>
-      <title>Ukas kjerneverdi</title>
-
-      <pre>{getCoreValue()}</pre>
-
-      <footer>
-        En tjeneste levert av <a href="http://vaffel.ninja/">VaffelNinja</a>
-      </footer>
-    </div>
-  )
+interface HomeProps {
+  coreValue: string
 }
+
+const Home: React.FC<HomeProps> = (props: HomeProps) => (
+  <>
+    <title>Ukas kjerneverdi</title>
+
+    <pre>{props.coreValue}</pre>
+
+    <footer>
+      En tjeneste levert av <a href="http://vaffel.ninja/">VaffelNinja</a>
+    </footer>
+  </>
+)
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => ({
+  props: {
+    coreValue: getCoreValue(),
+  },
+  revalidate: 3600,
+})
+
+export default Home
